f4903341a522a4280f0443dc711d238aaf92b0e6
vincentevangelsita529 <vincentevangelista529@gmail.com>
2026-07-23 12:47:26 -0700

    Switch Database connection to Neon PostgreSQL

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
